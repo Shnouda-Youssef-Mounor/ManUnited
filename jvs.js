940eacb2c5142baf42f34283f5f476a2b4049e7f
Shnouda-Youssef-Mounir <111351280+Shnouda-Youssef-Mounor@users.noreply.github.com>
2022-09-28 17:21:07 +0200

    Add files via upload

---
+++ jvs.js
@@ -0,0 +1,9 @@
+var myelement = document.getElementById('load'),
+    btr = document.getElementById('home');
+
+function changeTips() {
+    return setTimeout(() => {
+        myelement.innerHTML = "";
+    }, 2650);
+}
+changeTips();
